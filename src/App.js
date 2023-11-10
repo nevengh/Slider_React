ba77362db435f470dc8a87a457fe00a36f52eead
Neven <nevenghasoun@gmail.com>
2023-11-09 21:51:54 -1100

    sliderreact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+import image1 from "./assets/slide_1.jpeg";
+import image2 from "./assets/slide_2.jpeg";
+import image3 from "./assets/slide_3.jpeg";
+import image4 from "./assets/slide_4.jpeg";
+import image5 from "./assets/slide_5.jpeg";
+import image6 from "./assets/slide_6.jpeg";
+import ImageSlider from './Components/SliderImage/ImageSlider';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <ImageSlider images={[image1, image2, image3, image4, image5, image6]}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              color: "#fff",
+            }}
+          >
+            <h1>React.js Image Slider</h1>
+            <p>Nulla vitae elit libero, a pharetra augue.</p>
+          </div>
+        </ImageSlider>
+      </div>
     </div>
   );
 }
